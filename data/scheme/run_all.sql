4b2cca5e2fb87ad71b5450b70003d55f250a342b
Kristóf Bodroghy <bodroghykristof@gmail.com>
2021-04-03 14:57:16 +0200

    db: add pocket table

--- data/scheme/run_all.sql
+++ data/scheme/run_all.sql
@@ -1,6 +1,8 @@
 ALTER TABLE IF EXISTS ONLY public.atom DROP CONSTRAINT IF EXISTS pk_atom_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.atom_position DROP CONSTRAINT IF EXISTS pk_atom_position_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.atom_position DROP CONSTRAINT IF EXISTS fk_atom_position_atom_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.pocket DROP CONSTRAINT IF EXISTS pk_pocket_id CASCADE;
+
 
 DROP TABLE IF EXISTS public.atom_position;
 CREATE TABLE atom_position (
@@ -27,3 +29,32 @@ CREATE TABLE atom (
 
 ALTER TABLE ONLY atom_position
     ADD CONSTRAINT fk_atom_position_atom_id FOREIGN KEY (atom_id) REFERENCES atom(id) ON DELETE CASCADE;
+
+
+DROP TABLE IF EXISTS public.pocket;
+CREATE TABLE pocket (
+    id serial primary key,
+    snapshot integer,
+    score decimal,
+    druggability_score decimal,
+    alpha_spheres_no integer,
+    total_sasa decimal,
+    polar_sasa decimal,
+    apolar_sasa decimal,
+    volume decimal,
+    mean_local_hydrobhopic_density decimal,
+    mean_alpha_sphere_radius decimal,
+    mean_alpha_sphere_solvent_access decimal,
+    hydrophobicity_score decimal,
+    volume_score decimal,
+    polarity_score decimal,
+    charge_score decimal,
+    polar_atom_proportion decimal,
+    alpha_sphere_density decimal,
+    center_alpha_sphere_max_dist decimal,
+    flexibility decimal,
+    mean_b_factor decimal,
+    pocket_volume_monte_carlo decimal,
+    pocket_volume_convex_hull decimal,
+    apolar_alpha_sphere_no integer
+);
