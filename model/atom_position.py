1bb5598fd32daca9e5a8462af68f3a6028d69548
Kristóf Bodroghy <bodroghykristof@gmail.com>
2021-04-03 12:58:39 +0200

    model: add AtomPosition class

---
+++ model/atom_position.py
@@ -0,0 +1,9 @@
+class AtomPosition:
+
+    def __init__(self, id_, snapshot, atom_id, pos_x, pos_y, pos_z):
+        self.id_ = id_
+        self.snapshot = snapshot
+        self.atom_id = atom_id
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.pos_z = pos_z
