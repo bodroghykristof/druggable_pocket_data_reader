6f1a2067e7541e63d6eca1fd9900cf992873a188
Kristóf Bodroghy <bodroghykristof@gmail.com>
2021-04-03 11:03:46 +0200

    db: add atom table

---
+++ data/scheme/create_atom_table_20210403.sql
@@ -0,0 +1,13 @@
+ALTER TABLE IF EXISTS ONLY public.atom DROP CONSTRAINT IF EXISTS pk_atom_id CASCADE;
+
+DROP TABLE IF EXISTS public.atom;
+CREATE TABLE atom (
+    id serial primary key,
+    atom_type varchar(10),
+    amino_acid_name varchar(3),
+    protein_id integer,
+    amino_acid_id integer,
+    occupancy decimal,
+    temperature_factor decimal,
+    atom_symbol varchar(3)
+);
