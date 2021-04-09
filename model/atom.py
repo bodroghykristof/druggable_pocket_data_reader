ff57b5976c5a5ec761eab43d0aa403012466a6de
Kristóf Bodroghy <bodroghykristof@gmail.com>
2021-04-09 19:48:07 +0200

    fix: move time-dependent properties to atom_position from atom (model update)

--- model/atom.py
+++ model/atom.py
@@ -1,12 +1,9 @@
 class Atom:
 
-    def __init__(self, id_, atom_type, amino_acid_name, protein_id, amino_acid_id, occupancy,
-                 temperature_factor, atom_symbol):
+    def __init__(self, id_, atom_type, amino_acid_name, protein_id, amino_acid_id, atom_symbol):
         self.id_ = id_
         self.atom_type = atom_type
         self.amino_acid_name = amino_acid_name
         self.protein_id = protein_id
         self.amino_acid_id = amino_acid_id
-        self.occupancy = occupancy
-        self.temperature_factor = temperature_factor
         self.atom_symbol = atom_symbol
--- model/atom_position.py
+++ model/atom_position.py
@@ -1,8 +1,10 @@
 class AtomPosition:
 
-    def __init__(self, snapshot, atom_id, pos_x, pos_y, pos_z):
+    def __init__(self, snapshot, atom_id, pos_x, pos_y, pos_z, occupancy, temperature_factor):
         self.snapshot = snapshot
         self.atom_id = atom_id
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.pos_z = pos_z
+        self.occupancy = occupancy
+        self.temperature_factor = temperature_factor
