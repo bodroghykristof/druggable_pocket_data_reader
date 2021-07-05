848b63941c42238c62ad8a5a46c7243d96a8c843
Kristóf Bodroghy <bodroghykristof@gmail.com>
2021-07-05 15:56:16 +0200

    clear: removed unused test methods, comments

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from service import amino_acid_array_processor_service
 
-investigated_radius = 100
-repeat_number = 1200
-
 
 def main():
     amino_acid_array_processor_service.process_amino_acid_arrays()
