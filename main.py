b67537e527123ea5e4aeb18d80b2a831034b9c0f
Kristóf Bodroghy <bodroghykristof@gmail.com>
2021-08-28 14:42:15 +0200

    refactor: add comments and placeholder to main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+"""Main entry point of the application."""
+
+
 from service.amino_acid_array_processor import amino_acid_array_processor_service
+from service.data_reader import data_reader_service
 
 
 def main():
+    """Placeholder for running one of the 2 modules ot the applicaiton."""
+
+    #####################################
+    # ENTRY POINT OF DATA READER MODULE #
+    #####################################
+    data_reader_service.read_data()
+
+    ####################################################
+    # ENTRY POINT OF AMINO ACID ARRAY PROCESSOR MODULE #
+    ####################################################
     amino_acid_array_processor_service.process_amino_acid_arrays()
 
 
